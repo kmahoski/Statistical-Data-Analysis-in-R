6849da164c302a7a58d7be24e2bb303a0604bc39
Kliment Mahoski <kliment.mahoski@gmail.com>
2014-10-31 22:02:51 +0100

    Update Exercise 15.31.r

--- Chapter 15/Exercise 15.31.r
+++ Chapter 15/Exercise 15.31.r
@@ -5,3 +5,4 @@ confidenceLevel <- 0.95
 
 wilcox.test(currents1, currents2, alternative = "greater", conf.level = confidenceLevel,
 	conf.int = TRUE, paired = FALSE, exact = FALSE, correct = FALSE)
+
